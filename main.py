72bdb7905a00bee40d31afdf1ed8de1e2824415f
gianni <blackgbelt@gmail.com>
2024-03-23 14:33:20 -0400

    refactoring + extra testfiles

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import glob
 
 
 def take_files(folder: str) -> list[str]:
-    return glob.glob(folder + '/*.jpg')
+    return glob.glob(folder + '/*')
+
+
+def in_bounds(choice: int, length: int) -> bool:
+    return choice in range(0, length)
 
 
 def choose_file(folder: str, contents: list[str]) -> int:
@@ -12,33 +16,31 @@ def choose_file(folder: str, contents: list[str]) -> int:
         print(index, file, sep=': ')
 
     option: str = input("\nPick an option: ")
+
     try:
-        if 0 <= int(option) - 1 < len(contents):
+        if in_bounds(int(option) - 1, len(contents)):
             return int(option) - 1
         else:
             raise ValueError
     except ValueError:
-        invalid_scope = option.isnumeric() and not 0 <= int(option) - 1 < len(contents)
-
-        while invalid_scope or not option.isnumeric():
+        while (option.isnumeric() and not in_bounds(int(option) - 1, len(contents))) or not option.isnumeric():
             print("Invalid; please pick a valid option!")
             option = input("\nPick an option: ")
-
-            invalid_scope = option.isnumeric() and not 0 <= int(option) - 1 < len(contents)
 
     return int(option) - 1
 
 
-def convert_to_ascii(file: str) -> str:
-    chars: str = "@%#*+=-:. "
-
+def convert_to_ascii(file: str, new_height: int, chars="@%#*+=-:. ") -> str:
+    """
+    TODO: ASCII characters are taller than they are wider, I need to configure the algorithm with this in mind.
+    TODO: The calculation of width with respect to height must be modified.
+    """
     img: Image = Image.open(file)
     img = img.convert('L')
     with open('result.txt', 'w') as ret:
 
         width, height = img.size
-        aspect_ratio: float = height / width
-        new_height: int = 100
+        aspect_ratio: float = width / height
         new_width: int = int(aspect_ratio * new_height)
         img.thumbnail((new_width, new_height))
 
@@ -54,8 +56,29 @@ def convert_to_ascii(file: str) -> str:
         return ret.name
 
 
+def take_int_option(value: str, measurement: str) -> int:
+    choice: str = input(f"Enter {value} in {measurement}: ")
+    try:
+        return int(choice)
+    except ValueError:
+        while not choice.isnumeric():
+            choice = input(f"Not an integer!\n"
+                           f"Please re-enter {value} in {measurement}: ")
+    return int(choice)
+
+
+def take_bool_option(true: str, false: str) -> bool:
+    valid_choices: tuple[str, str] = (true.lower(), false.lower())
+    choice: str = input(f"Choose an option ({true}/{false}): ").lower()
+    while choice not in valid_choices:
+        choice = input("Invalid choice!\n"
+                       f"Please re-enter ({true}/{false}): ").lower()
+
+    return choice == true.lower()
+
+
 def main():
-    print("Hello! Welcome to ASCpii, a .jpg to ASCII converter!")
+    print("Hello! Welcome to ASCpii, an image to ASCII converter!")
     directory: str = input("\nEnter image folder name: ")
     files: list[str] = take_files(directory)
 
@@ -65,8 +88,13 @@ def main():
         files = take_files(directory)
 
     index: int = choose_file(directory, contents=files)
-    result_file = convert_to_ascii(files[index])
-    print(f"Image converted! Check {result_file}!")
+    converted_height: int = 100
+    print("\nWould you like to change the converted image height?\n"
+          "(default is 100px)")
+    if take_bool_option("Y", "N"):
+        converted_height: int = take_int_option("new height", "pixels")
+    result_file: str = convert_to_ascii(files[index], converted_height)
+    print(f"\nImage converted! Check {result_file}!")
 
 
 if __name__ == "__main__":
